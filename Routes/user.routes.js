3b2483675e4493e2042a59a2f64daee2b1c2534c
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-09-23 20:24:58 -0300

    Test de Auth

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -4,15 +4,20 @@ const router = express.Router()
 import { getAllUser, createUser, updateUser, deleteUser, singUp, loginUser, getUserByRut } from '../controllers/user.controller.js'
 import { authRequire } from '../middlewares/auth.middleware.js'
 
-router.get('/users', authRequire, getAllUser)
+// authRequire eliminado para test
+
+router.get('/users', getAllUser)
+
 //router.post('/users', createUser) se comenta por que sino no funciona el encriptado de PASSWORD
-router.put('/users/:rut', authRequire, updateUser)
-router.delete('/users/:rut', authRequire, deleteUser)
+router.put('/users/:rut', updateUser)
+
+router.delete('/users/:rut', deleteUser)
 
 router.post('/users', singUp)
+
 router.post('/login', loginUser)
 
-router.get('/users/:rut', authRequire, getUserByRut)
+router.get('/users/:rut', getUserByRut)
 
 
 export default router
