08fbc5bb5b7bf6a4f4f18585c6ad470994063da6
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-09-24 19:05:45 -0300

    Actualizacion de Cors

---
+++ middlewares/cors.middleware.js
@@ -0,0 +1,7 @@
+export const corsOptions = {
+    origin: [
+        // URL del Frontend
+        'http://localhost:4000'
+    ],
+    optionsSucessStatus: 200
+}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import userRouter from './Routes/user.routes.js'
 import { db } from './config/db.config.js'
+import cors from 'cors'
 
 import dotenv from 'dotenv'
+import { corsOptions } from './middlewares/cors.middleware.js'
 dotenv.config()
 
 const app = express()
@@ -10,8 +12,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Middlewares Cors
+app.use(cors(corsOptions))
 //Middlewares de rutas
 app.use('/api/v1', userRouter)
+
+
 
 db()
 
