95458bf335cb0d56af2c4d6e7f3f5f9acaf036b2
Raul Oyarzun <r.oyarzun83@gmail.com>
2023-10-04 20:11:17 -0300

    Mod cros Credentials

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -1,7 +1,8 @@
 export const corsOptions = {
     origin: [
         // URL del Frontend.
-        'https://proyecto-5-backend-eeei.onrender.com:4002'
+        'https://proyecto-5-backend-eeei.onrender.com:4002',
     ],
+    credentials: true,
     optionsSucessStatus: 200
 }
